048eceed0fd65bf02b1c2e114ef69629b9e4db47
Rajendra Kadam <rajendrakadam249@gmail.com>
2017-01-21 17:37:44 +0530

    Cyclic check in graph

--- Week 2/acyclicity.py
+++ Week 2/acyclicity.py
@@ -2,9 +2,43 @@
 
 import sys
 
+adjacent = []
 
 def acyclic(adj):
+    global adjacent
+    white_set = []
+    gray_set = []
+    black_set = []
+    adjacent = adj
+    for i in range(len(adj)):
+        white_set.append(i)
+
+    for item in white_set:
+        current = item
+        if dfs(current, white_set, gray_set, black_set):
+            return 1
     return 0
+
+def dfs(current, white_set, gray_set, black_set):
+    move_vertex(current, white_set, gray_set)
+
+    for vertex in adjacent[current]:
+        if vertex in black_set:
+            continue
+
+        if vertex in gray_set:
+            return True
+
+        if dfs(vertex, white_set, gray_set, black_set):
+            return True
+
+    move_vertex(current, gray_set, black_set)
+    return False
+
+
+def move_vertex(vertex, source, destination):
+    source.remove(vertex)
+    destination.append(vertex)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
